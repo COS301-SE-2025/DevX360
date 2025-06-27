8766c278e9cedd04ad95517efeb0ff3fd88c303e
David Musa-Aisien <cal24815@gmail.com>
2025-06-27 06:17:33 +0200

    Delete Testing directory

--- Testing/tests/unit/sample.test.js
+++
@@ -1,8 +0,0 @@
-
-const fs = require('fs');
-
-test('File starts with the word "test"', () => {
-  const content = fs.readFileSync('Testing/tests/unit/sampleText.txt', 'utf8');
-  const firstWord = content.trim().split(/\s+/)[0];
-  expect(firstWord.toLowerCase()).toBe('test');
-});
