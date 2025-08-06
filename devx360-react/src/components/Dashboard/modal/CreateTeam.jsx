9acb8e3fa45ffc1b2fc0071dde8a9d3a0a332fce
lwando04 <lwandomsindo@gmail.com>
2025-08-06 18:15:55 +0200

    Add skeleton modal components

---
+++ devx360-react/src/components/Dashboard/modal/CreateTeam.jsx
@@ -0,0 +1,9 @@
+
+
+function CreateTeamModal() {
+    return (
+        <></>
+    )
+}
+
+export default CreateTeamModal;
---
+++ devx360-react/src/components/Dashboard/modal/JoinTeam.jsx
@@ -0,0 +1,9 @@
+
+
+function JoinTeamModal() {
+    return (
+        <></>
+    )
+}
+
+export default JoinTeamModal;
