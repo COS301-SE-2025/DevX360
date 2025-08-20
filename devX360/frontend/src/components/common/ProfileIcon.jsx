bb7ecfc10013102046492e3155d1f23a8956c7d1
Sipho(u23535212) <u23535212@tuks.co.za>
2025-08-20 17:02:44 +0200

    Add files via upload

---
+++ devX360/frontend/src/components/common/ProfileIcon.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function ProfileIcon({ currentUser, avatar, defaultAvatar }) {
+    // console.log(currentUser);
+    return (
+        <div className="user-profile">
+            <div className="user-avatar">
+                <img
+                    className="user-avatar"
+                    src={avatar}
+                    alt="User Avatar"
+                    onError={(e) => {
+                        e.target.src = defaultAvatar;
+                    }}
+                />
+            </div>
+            <div className="user-info">
+                <span className="user-name">{currentUser?.name}</span>
+                <span className="user-role">{currentUser?.role}</span>
+            </div>
+        </div>
+    );
+}
+
+export default ProfileIcon;
