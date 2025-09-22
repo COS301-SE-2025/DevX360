850c8958ee9d2142aaad15be150f6c776cb84977
SibusisoMngo <u20441984@tuks.co.za>
2025-09-22 20:41:30 +0200

    test(mocks): add NavLink to react-router-dom mock for Sidebar tests

--- devx360-react/src/__mocks__/react-router-dom.js
+++ devx360-react/src/__mocks__/react-router-dom.js
@@ -4,5 +4,6 @@ module.exports = {
   MemoryRouter: ({ children }) => React.createElement('div', null, children),
   BrowserRouter: ({ children }) => React.createElement('div', null, children),
   useNavigate: () => () => {},
+  NavLink: ({ children }) => React.createElement('a', null, children),
   Link: ({ children }) => React.createElement('a', null, children),
 };
